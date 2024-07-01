508adffc5f4c7a423819f54876bff9e839343800
Alex Maldonado <alex@oasci.org>
2024-07-01 09:28:50 -0400

    fix: df_split

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,5 +6,5 @@ def test_split(path_example_prepped_csv):
     df = load_file(path_example_prepped_csv, file_type="csv")
     df_train, df_test = split_df(df=df, hours=24.0)
 
-    assert df_train.shape == (95, 3)
-    assert df_test.shape == (2_626, 3)
+    assert df_train.shape == (94, 3)
+    assert df_test.shape == (2_627, 3)
--- vaxstats/utils.py
+++ vaxstats/utils.py
@@ -9,19 +9,27 @@ from loguru import logger
 def split_df(
     df: pl.DataFrame,
     hours: float | int | Collection[float | int] = 72.0,
+    date_column: str = "ds",
+    date_fmt: str = "%Y-%m-%d %H:%M:%S",
     *args: tuple[Any, ...],
     **kwargs: dict[str, Any],
 ) -> tuple[pl.DataFrame, ...]:
     """
     Splits a DataFrame into multiple sets based on time window(s).
 
-    This function takes a Polars DataFrame with a timestamp column named 'ds',
-    parses the timestamps, and splits the DataFrame into $n$ parts. Each DataFrame is
-    also sorted from earliest to latest.
+    This function takes a Polars DataFrame with a timestamp column named `ds`,
+    parses the timestamps, and splits the DataFrame into $n$ parts.
 
     Args:
         df: The input DataFrame containing a 'ds' column with timestamps.
-        hours: The time window in hours for the training set.
+        hours: Time duration (exclusive bounds) to include in $n - 1$ splits. If this is
+            `24`, then two splits with the following rows will be provided:
+
+            -   Up to, but not including, `24` hours,
+            -   Any rows at and after `24` hours.
+        date_column: column name containing date information.
+        date_fmt: [Date format codes](https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes)
+            for date column.
         *args: Additional positional arguments.
         **kwargs: Additional keyword arguments.
 
@@ -79,21 +87,26 @@ def split_df(
         hours = (hours,)
     logger.info(f"Splitting DataFrame into {len(hours) + 1}")
 
-    df = df.with_columns(pl.col("ds").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S"))
-    earliest_time = df.select(pl.col("ds").min()).item()
+    df = df.with_columns(pl.col(date_column).str.strptime(pl.Datetime, date_fmt))
+    earliest_time = df.select(pl.col(date_column).min()).item()
     logger.debug(f"Earliest time found: {earliest_time}")
+
+    logger.debug("Sorting DataFrame by time")
+    df = df.sort(date_column)
 
     splits = []
     previous_time = earliest_time
     for hour in hours:
+        logger.debug(f"Finding lines before {previous_time}")
         time_window_end = previous_time + timedelta(hours=hour)
         df_split = df.filter(
-            (pl.col("ds") >= previous_time) & (pl.col("ds") < time_window_end)
-        ).sort("ds")
+            (pl.col(date_column) >= previous_time)
+            & (pl.col(date_column) < time_window_end)
+        )
         splits.append(df_split)
         previous_time = time_window_end
 
     # Handle the remaining data
-    df_remaining = df.filter(pl.col("ds") >= previous_time).sort("ds")
+    df_remaining = df.filter(pl.col(date_column) >= previous_time)
     splits.append(df_remaining)
     return tuple(splits)
