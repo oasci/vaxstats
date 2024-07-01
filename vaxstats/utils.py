b0194adb7288857930784dc9f5bfd669c1fa8a4b
Alex Maldonado <alex@oasci.org>
2024-07-01 09:05:09 -0400

    feat: improve splitting DataFrames

--- vaxstats/utils.py
+++ vaxstats/utils.py
@@ -1,42 +1,39 @@
-from typing import Any
+from typing import Any, Collection
 
 from datetime import timedelta
 
 import polars as pl
+from loguru import logger
 
 
 def split_df(
     df: pl.DataFrame,
-    hours: float | int = 72.0,
+    hours: float | int | Collection[float | int] = 72.0,
     *args: tuple[Any, ...],
     **kwargs: dict[str, Any],
-) -> tuple[pl.DataFrame, pl.DataFrame]:
+) -> tuple[pl.DataFrame, ...]:
     """
-    Splits a DataFrame into training and testing sets based on a time window.
+    Splits a DataFrame into multiple sets based on time window(s).
 
     This function takes a Polars DataFrame with a timestamp column named 'ds',
-    parses the timestamps, and splits the DataFrame into two parts:
-    training and testing sets. The training set includes rows from the earliest
-    timestamp up to a specified number of hours. The testing set includes the
-    remaining rows.
+    parses the timestamps, and splits the DataFrame into $n$ parts. Each DataFrame is
+    also sorted from earliest to latest.
 
     Args:
-        df (pl.DataFrame): The input DataFrame containing a 'ds' column with timestamps.
-        hours (float | int, optional): The time window in hours for the training set.
-            Defaults to 72.0.
-        *args (tuple[Any, ...]): Additional positional arguments.
-        **kwargs (dict[str, Any]): Additional keyword arguments.
+        df: The input DataFrame containing a 'ds' column with timestamps.
+        hours: The time window in hours for the training set.
+        *args: Additional positional arguments.
+        **kwargs: Additional keyword arguments.
 
     Returns:
-        tuple[pl.DataFrame, pl.DataFrame]: A tuple containing two DataFrames.
-            The first DataFrame is the training set, and the second DataFrame
-            is the testing set.
+        A tuple containing DataFrame splits.
 
     Raises:
         ValueError: If the 'ds' column is not found in the DataFrame.
         TypeError: If 'hours' is not a float or int.
 
-    Examples:
+     Examples:
+        ```python
         >>> import polars as pl
         >>> data = {
         ...     'ds': ["2023-01-01 00:00:00", "2023-01-02 00:00:00", "2023-01-03 00:00:00",
@@ -44,40 +41,59 @@ def split_df(
         ...     'value': [10, 20, 30, 40, 50]
         ... }
         >>> df = pl.DataFrame(data)
-        >>> df_train, df_test = split_df(df, hours=48)
-        >>> df_train
-        shape: (2, 2)
+        >>> splits = split_df(df, hours=[24, 48])
+        >>> len(splits)
+        3
+        >>> splits[0]
+        shape: (1, 2)
         ┌─────────────────────┬───────┐
         │ ds                  ┆ value │
         │ ---                 ┆ ---   │
         │ datetime[ms]        ┆ i64   │
         ╞═════════════════════╪═══════╡
         │ 2023-01-01 00:00:00 ┆ 10    │
-        │ 2023-01-02 00:00:00 ┆ 20    │
         └─────────────────────┴───────┘
-        >>> df_test
-        shape: (3, 2)
+        >>> splits[1]
+        shape: (2, 2)
         ┌─────────────────────┬───────┐
         │ ds                  ┆ value │
         │ ---                 ┆ ---   │
         │ datetime[ms]        ┆ i64   │
         ╞═════════════════════╪═══════╡
+        │ 2023-01-02 00:00:00 ┆ 20    │
         │ 2023-01-03 00:00:00 ┆ 30    │
+        └─────────────────────┴───────┘
+        >>> splits[2]
+        shape: (2, 2)
+        ┌─────────────────────┬───────┐
+        │ ds                  ┆ value │
+        │ ---                 ┆ ---   │
+        │ datetime[ms]        ┆ i64   │
+        ╞═════════════════════╪═══════╡
         │ 2023-01-04 00:00:00 ┆ 40    │
         │ 2023-01-05 00:00:00 ┆ 50    │
         └─────────────────────┴───────┘
-
+        ```
     """
+    if isinstance(hours, (int, float)):
+        hours = (hours,)
+    logger.info(f"Splitting DataFrame into {len(hours) + 1}")
+
     df = df.with_columns(pl.col("ds").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S"))
     earliest_time = df.select(pl.col("ds").min()).item()
-    time_window_end = earliest_time + timedelta(hours=hours)
+    logger.debug(f"Earliest time found: {earliest_time}")
 
-    df_train = df.filter(
-        (pl.col("ds") >= earliest_time) & (pl.col("ds") <= time_window_end)
-    )
-    df_train = df_train.sort("ds")
-    df_test = df.filter(
-        ~((pl.col("ds") >= earliest_time) & (pl.col("ds") <= time_window_end))
-    )
-    df_test = df_test.sort("ds")
-    return df_train, df_test
+    splits = []
+    previous_time = earliest_time
+    for hour in hours:
+        time_window_end = previous_time + timedelta(hours=hour)
+        df_split = df.filter(
+            (pl.col("ds") >= previous_time) & (pl.col("ds") < time_window_end)
+        ).sort("ds")
+        splits.append(df_split)
+        previous_time = time_window_end
+
+    # Handle the remaining data
+    df_remaining = df.filter(pl.col("ds") >= previous_time).sort("ds")
+    splits.append(df_remaining)
+    return tuple(splits)
