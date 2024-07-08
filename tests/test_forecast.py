ee50f41e8d377ce5b6a59ce4bae6fc89e7bf4e12
Alex Maldonado <alex@oasci.org>
2024-07-08 14:17:39 -0400

    tests: switch fitting method to CSS-ML

--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -4,17 +4,20 @@ from vaxstats.forecast import run_forecasting
 from vaxstats.io import load_file
 
 
-def test_arima(path_example_prepped_csv):
+def test_arima(path_example_prepped_csv, path_example_forecast_csv):
     model_kwargs = {
         "order": (0, 0, 10),
         "seasonal_order": (0, 1, 1),
         "season_length": 96,
-        "method": "ML",
+        "method": "CSS-ML",  # CSS-ML, ML, CSS
     }
     df = load_file(path_example_prepped_csv, "csv")
-    run_forecasting(
+    baseline_days = 7.0
+    baseline_hours = 24 * baseline_days
+    df = run_forecasting(
         df,
-        baseline_hours=25.0,
+        baseline_hours=baseline_hours,
         sf_model=ARIMA,
         sf_model_kwargs=model_kwargs,
     )
+    df.write_csv(file=path_example_forecast_csv)
