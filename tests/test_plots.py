654494c96f6b5645471fa15964776c544f949267
Alex Maldonado <alex@oasci.org>
2024-07-11 21:44:58 -0400

    feat: add plot_forecast

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,5 +1,5 @@
 from vaxstats.io import load_file
-from vaxstats.plots import plot_data_line
+from vaxstats.plots import plot_data_line, plot_forecast
 from vaxstats.utils import datetime_to_float
 
 
@@ -8,4 +8,13 @@ def test_data_plot(path_example_prepped_csv, path_example_img):
     temps = df["y"].to_numpy()
     days = datetime_to_float(df, time_unit="days")
     fig = plot_data_line(days, temps)
+    fig.savefig(path_example_img)  # type: ignore
+
+
+def test_data_forecast_plot(path_example_forecast_csv, path_example_img):
+    df = load_file(path_example_forecast_csv, file_type="csv")
+    temps = df["y"].to_numpy()
+    temps_pred = df["y_hat"].to_numpy()
+    days = datetime_to_float(df, time_unit="days")
+    fig = plot_forecast(days, temps, temps_pred)
     fig.savefig(path_example_img)  # type: ignore
--- vaxstats/plots.py
+++ vaxstats/plots.py
@@ -19,3 +19,22 @@ def plot_data_line(
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     return fig  # type: ignore
+
+
+def plot_forecast(
+    time: npt.NDArray[np.float64],
+    y: npt.NDArray[np.float64],
+    y_hat: npt.NDArray[np.float64],
+    xlabel: str = "Time",
+    ylabel: str = "Temperature [°C]",
+    *args: tuple[Any, ...],
+    **kwargs: dict[str, Any],
+) -> mpl.figure.Figure:
+    fig, ax = plt.subplots(nrows=1, ncols=1, tight_layout=True, **kwargs)  # type: ignore
+    ax.plot(time, y, label="True")
+    ax.plot(time, y_hat, label="Forecast")
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.legend(frameon=False)
+
+    return fig  # type: ignore
